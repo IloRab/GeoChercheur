55c0c0be629604f096c78a0365e4ffa62b3dd266
yousrah24 <118174920+yousrah24@users.noreply.github.com>
2022-12-09 12:06:04 +0100

    Update ajout_Parcours.php

--- PHP/ajout_Parcours.php
+++ PHP/ajout_Parcours.php
@@ -6,3 +6,10 @@ $data = file_get_contents($file);
 $obj = json_decode($data); 
 // accéder à l'élément approprié
 require("connectServer.php");
+// Récupération des variables à l'aide du client MySQL
+$usersStatement = $mysqlClient->prepare('CALL addParcours(?,?,?,?)');
+$usersStatement->bindParm(1,$obj->nom_parcours);
+$usersStatement->bindParm(2,$_SESSION['logged_user]['idClient']);
+$usersStatement->bindParm(3,$obj->description_parcours);
+$usersStatement->bindParm(4,$obj->thumbnail);
+$usersStatement->execute();
