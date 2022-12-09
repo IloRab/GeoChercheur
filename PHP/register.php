62d494b63bc179b803dcce5c356c1e6aa640c124
yousrah24 <118174920+yousrah24@users.noreply.github.com>
2022-12-09 12:22:51 +0100

    Update register.php

--- PHP/register.php
+++ PHP/register.php
@@ -1,18 +1,19 @@
 <?php
 $pseudo = isset($_POST('pseudo')) ? $_POST('pseudo') : "";
-$password = isset($_POST('lat')) ? $_POST('lien') : "";
+$password = isset($_POST('password')) ? $_POST('password') : "";
 
 if($pseudo != "" && $password != ""){
-    require("variables.php");
-    $sql = $mysqlClient->prepare('CALL addClient(?,?)');
+    require("connectServer.php");
+    $sql = $mysqlClient->prepare('CALL addClient(?,?,?)');
     $sql->bindParam(1,$pseudo);
+    $sql->bindParam(2,$password);
     $sql->bindParam(2,$password);
     $sql->excute();
     $message = "Vous avez bien été inscrit !"
-    require("acceuil_connexion.php");
+    require("acceuil_connecte.php");
 }
 else{
-    require("");
+   require("acceuil_connexion.php");
 }
 
 ?>
