771f2f9272ab3ec45594147b699d3705c23d4cca
yousrah24 <souleyousrah@gmail.com>
2022-12-19 19:47:24 +0100

    mise à jour du formulaire inscription

---
+++ PHP/getDataPlayer.php
@@ -0,0 +1,35 @@
+<?php
+    $idClient = isset($_SESSION['LOGGED_USER']['idClient'])? $_SESSION['LOGGED_USER']['idClient'] : "";
+    $icon = isset($_SESSION['LOGGED_USER']['icon']) ? $_SESSION['LOGGED_USER']['icon'] : "assets/img/doggos/pug.jpg";
+    if($idClient != "")
+    {
+        require("connectServer.php");
+        try{
+            $usersStatement = $mysqlClient->prepare('SELECT SUM(scoreTotal) AS score FROM Scoretotal where idClient = ?');
+            $usersStatement->bindParam(1,$idClient);
+            $usersStatement->execute();
+            $player = $usersStatement->fetchAll();
+            $array = array(
+                "icon" => $icon,
+                "score" => $player['score'],
+            );
+            header('Content-Type: application/json;charset=utf-8');
+            echo json_encode($array);
+            exit();
+        }
+        catch(Exception $exception){
+            die('Erreur : '.$exception->getMessage());
+            
+        }
+    }else{
+        $array = array(
+            "icon" => $icon,
+            "score" => 0,
+        );
+        header('Content-Type: application/json;charset=utf-8');
+        echo json_encode($array);
+        exit();
+    }
+
+
+?>
