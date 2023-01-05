6cff3e80bcc3064fb50618289dc1203aadcb2608
Adrien Deü <75019689+AdrienDeu@users.noreply.github.com>
2023-01-05 19:01:33 +0100

    Insert_a_faire
    Les insert à faire pour la démonstration

---
+++ SQL/insert.sql
@@ -0,0 +1,5 @@
+CALL addQuestion(48.85733152620841, 2.292700000003265, 1); /* tour Eiffel */
+CALL addQuestion(48.873702, 2.295143, 1); /* arc de Triomphe */
+CALL addQuestion(48.846582183611, 2.3444420377778, 1); /* Panthéon */
+CALL addParcours("monuments de Paris", 1, "3 des plus beaux monuments de Paris", "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4b/La_Tour_Eiffel_vue_de_la_Tour_Saint-Jacques%2C_Paris_ao%C3%BBt_2014_%282%29.jpg/1200px-La_Tour_Eiffel_vue_de_la_Tour_Saint-Jacques%2C_Paris_ao%C3%BBt_2014_%282%29.jpg");
+
